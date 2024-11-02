9492873fab173e99c3df18c64950a599a08ce3cf
Venita Kissell <venitak@hotmail.com>
2024-11-02 21:53:51 +1000

    feat: finished the remove button function.

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -13,39 +13,52 @@ let dataArray = [
     "Clowns"
 ];
 
-
 let costumesContainer = document.getElementById("costumeContainer");
-// create an unorder list tag
-let costumesContainerList = document.createElement("ul");
-    // costumesContainer.innerText = "Hello world";
-
-dataArray.forEach((costume) => {
-
-    // verfiy what we are working with
-    console.log(costume);
-    // add the vlaues to the HTML element
-
-    // create the list element
-    let newCostumeEntry = document.createElement("li");
-
-    // add the value to the HTML element
-    newCostumeEntry.innerText = costume;
-
-    // add a button to each entry to remove from the list
-    let removeButton = document.createElement("button");
-
-    // add a placeholder value /name to the button
-    removeButton.innerText = `Remove ${costume}`;
 
 
-    // add the element 'li' to the container you must attach/append it to the root element
-    costumesContainerList.appendChild(newCostumeEntry);
+function renderData() {
 
-    // add the element 'li' to the container you must attach/append it to the root element
-    costumesContainerList.appendChild(removeButton);
+    // create an unorder list tag
+    let costumesContainerList = document.createElement("ul");
+
+    // removes the existing content from the element.
+    costumesContainer.innerText = "";
+
+    dataArray.forEach((costume) => {
+
+        // verfiy what we are working with
+        console.log(costume);
+   
+        // create the list element
+        let newCostumeEntry = document.createElement("li");
+
+        // add the value to the HTML element
+        newCostumeEntry.innerText = costume;
+
+        // add a button to each entry to remove from the list
+        let removeButton = document.createElement("button");
+
+        // add a placeholder value /name to the button
+        // removeButton.innerTest = "Remove" + costume;
+        removeButton.innerText = `Remove ${costume}`;
+
+        removeButton.onclick = (() => removeCostumeFromDataList(costume));
+
+        // add the element 'li' to the container you must attach/append it to the root element
+        costumesContainerList.appendChild(newCostumeEntry);
+
+        // add the element 'li' to the container you must attach/append it to the root element
+        newCostumeEntry.appendChild(removeButton);
 
 });
 
+    // Add the container to the HTML page append the 'ul' to the HTML page
+    costumesContainer.appendChild(costumesContainerList);
+}
 
-// Add the container to the HTML page append the 'ul' to the HTML page
-costumesContainer.appendChild(costumesContainerList);
+function removeCostumeFromDataList (targetItemToRemove) {
+
+    // Create a replica of the dataArray and remove the item that gets removed.
+    dataArray = dataArray.filter((costume) => costume != targetItemToRemove);
+    renderData();
+}
