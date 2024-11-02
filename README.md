b78a24fb94e3a656213b8208fb482f9ba2dc74ae
Venita Kissell <venitak@hotmail.com>
2024-11-02 20:10:42 +1000

    feat: simple js integration and dom manipulation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # t3w3-tuesday
+# JS Dom
+- finding and reading elements (html tags are called elements)
+- creating and deleting (using javascript)
+- editing elements
+
+
---
+++ src/scripts/dataList.js
@@ -0,0 +1,22 @@
+// Declare the data to display in the front-end
+let dataArray = [
+    "Frankenstein",
+    "Dracula",
+    "Vampire",
+    "Chucky",
+    "Max Verstappen",
+    "Wednesday",
+    "Voldemort",
+    "T-Rex",
+    "Gremlin Gizmo",
+    "Joker",
+    "Clowns"
+];
+
+dataArray.forEach((costume) => {
+    console.log(costume);
+})
+
+let costumesContainer = document.getElementById("costumeContainer");
+
+costumesContainer.innerText = "Hello world";
