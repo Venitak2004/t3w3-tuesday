a2d7d31a3b34e16b02359c5dff1c4adb41261f3e
Venita Kissell <venitak@hotmail.com>
2024-11-02 20:55:59 +1000

    feat: list added to HTML using JS DOM

--- src/scripts/dataList.js
+++ src/scripts/dataList.js
@@ -13,10 +13,28 @@ let dataArray = [
     "Clowns"
 ];
 
-dataArray.forEach((costume) => {
-    console.log(costume);
-})
 
 let costumesContainer = document.getElementById("costumeContainer");
+// create an unorder list tag
+let costumesContainerList = document.createElement("ul");
+    // costumesContainer.innerText = "Hello world";
 
-costumesContainer.innerText = "Hello world";
+dataArray.forEach((costume) => {
+
+    // verfiy what we are working with
+    console.log(costume);
+    // add the vlaues to the HTML element
+
+    // create the list element
+    let newCostumeEntry = document.createElement("li");
+
+    // add the value to the HTML element
+    newCostumeEntry.innerText = costume;
+
+    // add the element 'li' to the container you must attach/append it to the root element
+    costumesContainerList.appendChild(newCostumeEntry);
+});
+
+
+// Add the container to the HTML page append the 'ul' to the HTML page
+costumesContainer.appendChild(costumesContainerList);
